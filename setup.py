621e3506be91476dab6ae6ca70d2dab37ddc88fb
Jack Kelly <jack-list@xlk.org.uk>
2013-01-22 17:07:29 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "powerstats",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ['matplotlib', 'numpy', 'argparse'],
+    author = "Jack Kelly",
+    author_email = "",
+    description = "Create simple statistics from power data",
+    license = "MIT",
+    keywords = "power statistics python",
+    url = "https://github.com/JackKelly/powerstats/",
+    download_url = "https://github.com/JackKelly/powerstats/tarball/master"
+)
