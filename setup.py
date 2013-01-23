d20707d1df9a9227177a2315c737b0750598ab27
Jack Kelly <jack-list@xlk.org.uk>
2013-01-23 14:56:04 +0000

    Changing directory structure

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name = "powerstats",
-    version = "0.11",
+    version = "0.1",
     packages = find_packages(),
-    install_requires = ['matplotlib', 'numpy', 'argparse'],
+    install_requires = ['matplotlib', 'numpy'],
     author = "Jack Kelly",
-    author_email = "",
+    author_email = "jack-list@xlk.org.uk",
     description = "Create simple statistics from power data",
     license = "MIT",
     keywords = "power statistics python",
     url = "https://github.com/JackKelly/powerstats/",
-    download_url = "https://github.com/JackKelly/powerstats/tarball/master"
+    download_url = "https://github.com/JackKelly/powerstats/tarball/master#egg=powerstats-dev",
+    long_description = open('README.md').read()
 )
